fa8ca2a6fe589d09eed15694950422e156af7a9a
unknown <javid.asadli@outlook.com>
2020-04-12 16:28:14 +0200

    updated course content

--- Section3_Lecture5/normalize.rb
+++ Section3_Lecture5/normalize.rb
@@ -3,4 +3,7 @@
 # https://www.liblognorm.com/download/
 # yum install liblognorm
 # yum install rsyslog-mmnormalize
-# <133>1 2020-04-11T11:28:51.990206-04:00 rsyslog-client forward_tcp514 1000 - [version="5" environment="PROD" class="security_01"] message
+# <133>1 2020-04-11T11:28:51.990206-04:00 rsyslog-client forward_tcp514 1000 - [version="5" environment="PROD" class="security_01"] This is message
+version=2
+#rule=:%<field_name>:<field_type>{<parameters>}%
+rule=:<%rsyslog_priority:number%>1 %rsyslog_time:date-rfc5424% %rsyslog_host:word% %rsyslog_app:word% %rsyslog_procid% %rsyslog_msgid:word% [version="5" environment=%rsyslog_env:op-quoted-string% class=%rsyslog_class:op-quoted-string%] %rsyslog_message:rest%
